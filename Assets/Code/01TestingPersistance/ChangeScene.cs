f9283f73ef3d1d44b30adf44d650908fb0142710
Sam Hernandez <samueljohnhernandez@gmail.com.au>
2018-04-10 12:14:04 +1000

    start of my library
    this library will be a bunch of little tricks and tips that I figure out and can later reference

---
+++ Assets/Code/01TestingPersistance/ChangeScene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Button button;
+    public Text counterText;
+    int sceneCounter = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+
+        if (scene.name == "01TestingPersistanceScene1")
+        {
+            SceneManager.LoadScene(sceneName: "01TestingPersistanceScene2");
+            sceneCounter++;
+        }
+
+        if (scene.name == "01TestingPersistanceScene2")
+        {
+            SceneManager.LoadScene(sceneName: "01TestingPersistanceScene1");
+            sceneCounter++;
+        }
+
+        
+        counterText.text = ("Counter: " + sceneCounter);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
---
+++ Assets/Code/GeneralUse/DoNotDestroyOnLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoNotDestroyOnLoad : MonoBehaviour {
+    public static DoNotDestroyOnLoad control;
+ 
+
+
+    // Use this for initialization
+    void Start () {
+
+        //this maintains persistance of objects between scenes without creating multiple instances of the same object
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
