849eadb09aba5deb2758b1508feef681fa406fa8
Sam Hernandez <samueljohnhernandez@gmail.com.au>
2018-04-11 15:17:32 +1000

    tidied up examples, added reset button to collision example, added descriptions

---
+++ Assets/Code/GeneralUse/ReloadScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReloadScene : MonoBehaviour {
+
+    public Button button;
+
+    // Use this for initialization
+    void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick ()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(sceneName: scene.name);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
